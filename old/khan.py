12b5623533e3a3e94679c2e0bec20b4faba8f068
root <root@localhost.localdomain>
2018-11-09 00:18:53 +0000

    Refactoring and Organizing

---
+++ old/khan.py
@@ -0,0 +1,167 @@
+import hashlib, os, time, socket, threading
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    pass
+
+
+def tcp_client(host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    client.send("GET /HTTP/1.1\r\n\r\n")
+    response = client.recv(4096)
+    client.close()
+    return response
+
+
+def udp_client(host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.sendto("AAABBBCCC",(host, port))
+    data, address = client.recvfrom(4096)
+    client.close()
+    return data, address
+
+
+def whoAmI():
+    # os.system("sudo su")
+    os.system("touch self.txt")
+    os.system("ifconfig >> self.txt")
+    f = open("self.txt", "r")
+    ip = ""
+    mac = ""
+    interfaces = []
+    donuthin = 0
+    for ln in f.readlines():
+        #print(ln)
+        try:
+            ip = ln.split("inet ")[1].split("netmask")[0]
+            mac = ln.split("ether ")[1].split("tx")[1]
+            interfaces.append(ln.split(": flags")[0])
+        except:
+            donuthin += 1
+    os.system("rm self.txt")
+    # Now Get External IP
+    f2 = open("whoAmI.txt","r")
+    extIP = ""
+    for line in f2.readlines():
+        extIP+=line
+
+    return ip, mac, interfaces, extIP
+
+
+def run(ip,gpioflag):
+    TCPServer(ip,gpioflag)
+
+
+class TCPServer:
+    """
+    <TCP SERVER>
+    """
+    ACK_N = 0
+    ip = ""
+    Hello = False
+
+    def __init__(self, local,gpio):
+        """
+            If GPIO is connected, this program assumes LED pinout:
+            GPIO 20 :  Green LED
+            GPIO 24 :  Red LED
+            GPIO 27 :  Blue LED
+            :param local:
+            :param gpio:
+            """
+        self.hasLeds = gpio
+        if self.hasLeds:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(20, GPIO.OUT)  # Green
+            GPIO.setup(24, GPIO.OUT)  # Red
+            GPIO.setup(27, GPIO.OUT)  # Blue
+            # Server is running, to the light is GREEN
+            GPIO.output(20, GPIO.HIGH)
+
+        os.system("touch peers.txt")
+        self.ip = local
+        self.bind_ip = "0.0.0.0"
+        self.bind_port = 9999
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.bind_ip, self.bind_port))
+        server.listen(5)
+
+        go = True
+        print("[<<|- Starting P2P Node @ %s:%d-|>>]" % (self.bind_ip, self.bind_port))
+        while go and not self.Hello:
+                try:
+                    client, addr = server.accept()
+                    print "Connection Accepted from %s:%d" % (addr[0], addr[1])
+                    client_handler = threading.Thread(target=self.handle_client,
+                                                      args=(client,addr[0]))
+                    client_handler.start()
+                    if self.hasLeds:
+                        GPIO.output(20, GPIO.HIGH)
+                except KeyboardInterrupt:
+                    print "P2P Server Killed"
+                    if self.hasLeds:
+                        GPIO.output(27, GPIO.LOW)
+                        GPIO.output(20, GPIO.LOW)
+                    go = False
+        print "Handshake Completed"
+        os.system("rm peers.txt")
+        if self.hasLeds:
+            GPIO.cleanup()
+
+    def handle_client(self, client_socket, remote_host):
+        """
+        Client handling thread
+        :return:
+        """
+        if self.hasLeds:
+            GPIO.output(24, GPIO.HIGH)
+        request = client_socket.recv(1024)
+        print("[*- %s" % request)
+        # Send back an acknowledgement
+        # including what peer you are
+        # message = "ACK " + str(self.ACK_N)
+        message = hashlib.sha256(self.ip).hexdigest()
+        client_socket.send(message)
+        data, peer = self.client_side_p2p(client_socket,remote_host)
+        self.ACK_N += 1;
+
+    def client_side_p2p(self, me, peer):
+        if self.hasLeds:
+            GPIO.output(24, GPIO.LOW)
+            GPIO.output(20, GPIO.LOW)
+            GPIO.output(27, GPIO.HIGH)
+
+        peerHello = hashlib.sha256(self.ip).hexdigest()
+        me.sendto(peerHello, (peer, 9999))
+        data, address = me.recvfrom(4096)
+        me.close()
+        self.Hello = True
+        return data, address
+
+
+def main():
+    hasLED = False
+    try:
+        import RPi.GPIO as GPIO
+        hasLED = True
+    except ImportError:
+        print "Cannot Use GPIO for LEDs "
+    start = time.time()
+    running = True
+
+    # Get basic info about local machine for peer connections
+    ip, mac, interfaces, extIP = whoAmI()
+    print("Host IP: " + ip)
+    print("MAC(s): " + mac)
+    print("External IP is:\n" + extIP)
+
+    # Start a TCP Server to initialize Peer on the Network
+    run(ip, hasLED)
+    dt = time.time() - start
+    print(str(dt)+"s Elapsed")
+
+
+if __name__ == "__main__":
+    main()
---
+++ old/p2pext.py
@@ -0,0 +1,69 @@
+import hashlib, os, sys, time, socket, threading
+
+
+def whoAmI():
+    # os.system("sudo su")
+    os.system("touch self.txt")
+    os.system("ifconfig >> self.txt")
+    f = open("self.txt", "r")
+    ip = ""
+    mac = ""
+    interfaces = []
+    donuthin = 0
+    for ln in f.readlines():
+        #print(ln)
+        try:
+            ip = ln.split("inet ")[1].split("netmask")[0]
+            mac = ln.split("ether ")[1].split("tx")[1]
+            interfaces.append(ln.split(": flags")[0])
+        except:
+            donuthin += 1
+    os.system("rm self.txt")
+    return ip, mac, interfaces
+
+
+def tcp_client(host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    client.send("HI")
+    response = client.recv(4096)
+    client.close()
+    print(response)
+    return response
+
+
+def tcp_messenger(message,where,to,):
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((where, to))
+    sock.send(message)
+    print "Sending Message To: " +where
+    response = sock.recv(1024)
+    sock.close()
+    return response
+
+
+def usage():
+    print "**************************"
+    print "*| python p2pext.py <ip> |*"
+    print "**************************"
+
+
+def main():
+    start = time.time()
+    print "Connecting to P2P Network"
+    #get_external_ip()
+
+    if len(sys.argv) > 1:
+        peerIP = sys.argv[1]
+
+        ip, mac, interfaces = whoAmI()
+        # peerID = tcp_client(peerIP,9999)
+        myID = hashlib.sha256(ip).hexdigest()
+        print tcp_messenger(myID,peerIP,9999)
+        print "["+str(time.time() - start)+"s elapsed]"
+    else:
+        usage()
+
+
+if __name__ == "__main__":
+    main()
